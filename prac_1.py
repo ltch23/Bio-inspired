bce68f484e8d5c57c3801bcc5437e6cef04506be
ltch23 <luistong23@gmail.com>
2019-03-21 16:48:49 -0500

    final pract1

--- prac_1.py
+++ prac_1.py
@@ -1,26 +1,28 @@
 import math
 import numpy as np
+import random
 
-def shanon(vect):
+def shannon(vect):
 	suma = 0.0
 	for i in range(len(vect)):
 		suma = suma + (vect[i]* np.log2(vect[i]))
-	return -1*suma;
+	print("shannon: ",-1*suma)
 	
 def hartley(vect):
-	return np.log2(len(vect))
-
-#vector=np.array([0.5,0.2,0.1,0.1,0.1])
-#vector_2=np.array([0.2,0.2,0.2,0.2,0.2])
+	print("hartley: ", np.log2(len(vect)))
 
 
 def get_frecuency():
 	file_tmp= open("fin.txt","r")
 	file= open("fin.txt","r")
-		
+
+	#obtener tamaño de texto	
 	lenfile=len(file_tmp.read())
 
+	#donde se pondra las frecuencias 
 	vec_frec=np.empty(0)
+	
+	#similar a un contador de palabras
 	dicc={}
 	for line in file:
 		for char in line.lower(): 
@@ -32,15 +34,34 @@ def get_frecuency():
 		#print(k,v)
 		vec_frec=np.append(vec_frec,v)
 
-	#normalizar)
-	vect_frec_fin=np.true_divide(vec_frec,lenfile)
-	#print(vect_frec_fin)
-	return vect_frec_fin
+	#normalizar valores entre 0 y 1
+	#vect_frec_fin=np.true_divide(vec_frec,lenfile)
+	
+	# return vect_frec_fin
+	return vec_frec
+
+
+def gener():
+	file= open("fout.txt","w+")
+
+	list_char="abcdefghijklmnopqrstuvwxyz "
+	for i in range (40000):
+		char_sel=random.choice(list_char)
+		file.write(char_sel)
+	
+
+def permut():
+	file_tmp= open("fin.txt","r")
+	file= open("fout2.txt","w+")
+	data= list(file_tmp.read())
+	
+	for i in range (40000):
+		char_sel= random.choice(data)
+		data.remov(echar_sel)
+		file.write(char_sel)
 
 vector_frec=get_frecuency()
 
-print("shanon: ",shanon(vector_frec))
-print("hartley: ",hartley(vector_frec))
-
-
+shannon(vector_frec)
+hartley(vector_frec)
 
