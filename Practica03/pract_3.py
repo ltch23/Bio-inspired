ffb84909425bf6ca4cb5aaa6712b7e304979ddd5
ltch23 <luistong23@gmail.com>
2019-04-08 14:18:48 -0500

    nexting

--- Practica03/pract_3.py
+++ Practica03/pract_3.py
@@ -6,40 +6,69 @@ dig=6
 left = -100
 right = 100
 l=0
+m_size=10
 def individuo():
 	n_bin = np.random.randint(2, size=(l*2,))
-	num_r=codBintoR(n_bin)
-	return num_r
+	# num_r=codBintoR(n_bin)
+	# return num_r
+	return n_bin
+def popul(size):
+	return np.array([individuo() for i in range(size)])
 
 def gen_l(p):
 	return m.ceil(m.log2(right-left)+p*m.log2(10))
 
 def codBintoR(n_bin):
-	print("bin: ",n_bin)
+	# print("bin: ",n_bin)
 	rpta1=rpta2=0
 	for i in range(l):
 		rpta1 += (2**i)*n_bin[i]
 	rpta1 = left+rpta1*((right-left)/((2**l)-1))
-	print("R1: ",rpta1)
+	# print("R1: ",rpta1)
 
 	for i in range(l,l*2):
-		rpta2 += (2**i)*n_bin[i]
+		rpta2 += (2**(i-l))*n_bin[i]
 	rpta2 = left+rpta2*((right-left)/((2**l)-1))
-	print("R2: ",rpta2)
+	# print("R2: ",rpta2)
 	return [rpta1,rpta2]
 
-def function(x):
+def function(x_bin):
+	x=codBintoR(x_bin)
 	pow_xs = x[0]**2 + x[1]**2
 	num = m.sin(pow_xs)**2-0.5
 	den = (1.0+0.001*pow_xs)**2
 	return 0.5 - num/den
 
+def functions(xs):
+	# print(xs)
+	# print(xs.shape[0])
+	# print(xs[9,:])
+	arr=np.zeros(m_size)
+
+	all_x=np.zeros((m_size,2*l+1))
+	for i in range(xs.shape[0]):
+		arr[i]=function(xs[i])
+		all_x[:,:-1] = xs[i]
+		all_x[i,56] = arr[i]
+	# print("xs: ",xs[1].shape)
+	# print(all_x[0,56])
+
+	# print("all: ",all_x)
+	# print("all: ",all_x[:,:-1] )
+	return arr,all_x
+
 if __name__ == '__main__':
-	l=gen_l(dig)
-	x=individuo()
-	print("x: ",x)
-	fx=function(x)
-	print("fxs: ",fx)
+	l = gen_l(dig)
+	gen_x = popul(m_size)
+	gen_fx,all_x = functions(gen_x)
+	# print("all_x\n",gen_fx)
+	print("all\n",all_x)
+	# print("all\n",all_x[0])
+	print(all_x[0,56])
+	# print("sum: ", sum(all_x[:,56]))
+	# print("x: ",x_bin)
+	# fx=function(x_bin)
+	# print("fxs: ",fx)
 
 	# print(codBintoR())
 	
